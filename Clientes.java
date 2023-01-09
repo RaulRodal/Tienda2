ae982cfd38930abbcb295fa9712057802c81c555
craschelf <122305033+craschelf@users.noreply.github.com>
2023-01-09 18:52:03 +0100

    Create Clientes.java

---
+++ Clientes.java
@@ -0,0 +1,45 @@
+public class Clientes {
+    private String nombre;
+    private int telefono;
+    private String dni;
+    private String direccion;
+
+    public Clientes(String nombre, int telefono, int dni, String direccion) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.dni = dni;
+        this.direccion = direccion;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public void setDireccion(String direccion) {
+        this.dirección = dirección;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public String getDireccion() {
+        return dirección;
+    }
+}
