7e3bca3913145ac1be6c11cb9794e728168878ad
damdu112 <youremail>
2023-01-12 16:58:15 +0100

    añadidos comentarios

--- Tienda/src/tienda/Tienda.java
+++ Tienda/src/tienda/Tienda.java
@@ -9,7 +9,10 @@ package tienda;
  * @author damdu108
  */
 public class Tienda {
-
+    
+    // añadir todo el inventario en un Array de 100 elementos, se debe buscar, añadir y borrar
+    // borrado logico (atributo borrado) o array de historicos borras y vuelves a recalcular posiciones
+    
     /**
      * @param args the command line arguments
      */
